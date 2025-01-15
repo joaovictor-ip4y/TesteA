b11dff68d01e8be97e09806b28d94dc6d1683e50
joaovictor-ip4y <joao.victor@ip4y.com.br>
2025-01-15 15:41:37 -0300

    teste

---
+++ tests/ExampleTest.php
@@ -0,0 +1,11 @@
+// tests/ExampleTest.php
+<?php
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testAddition()
+    {
+        $this->assertEquals(2 + 2, 4);
+    }
+}
